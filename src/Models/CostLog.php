c3a8d55d3f39638e606a441fdc846a36de92576d
shawnveltman <shawnveltman@users.noreply.github.com>
2023-11-26 01:52:07 +0000

    Fix styling

--- src/Models/CostLog.php
+++ src/Models/CostLog.php
@@ -4,12 +4,10 @@ namespace Shawnveltman\LaravelOpenai\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class CostLog extends Model
 {
     use HasFactory;
 
     public $guarded = [];
-
 }
