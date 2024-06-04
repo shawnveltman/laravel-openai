929dd1e49649e225d0029be2c4394238673d0098
shawnveltman <shawnveltman@users.noreply.github.com>
2024-06-04 21:58:50 +0000

    Fix styling

--- config/ai_providers.php
+++ config/ai_providers.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'open_ai_key'   => env('OPEN_API_KEY'),
-    'mistral_key'   => env('MISTRAL_API_KEY'),
+    'open_ai_key' => env('OPEN_API_KEY'),
+    'mistral_key' => env('MISTRAL_API_KEY'),
     'anthropic_key' => env('ANTHROPIC_API_KEY'),
-    'gemini_key'    => env('GEMINI_API_KEY'),
+    'gemini_key' => env('GEMINI_API_KEY'),
 ];
